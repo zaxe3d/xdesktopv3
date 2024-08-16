3985f263424454e396d89bc363bc6a5670e6e33a
Gokhan Onis <gokhanonis@gmail.com>
2024-08-16 13:05:15 +0300

    added copyright info.

--- src/slic3r/GUI/NetworkMachineManager.hpp
+++ src/slic3r/GUI/NetworkMachineManager.hpp
@@ -1,3 +1,7 @@
+///|/ Copyright (c) Zaxe 2018 - 2024 Gökhan Öniş @GO
+///|/
+///|/ XDesktop is released under the terms of the AGPLv3 or higher
+///|/
 #ifndef slic3r_NetworkMachineManager_hpp_
 #define slic3r_NetworkMachineManager_hpp_
 
